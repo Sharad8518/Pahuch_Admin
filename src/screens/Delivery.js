57f4451ecf5799281a0c3ebec70a5e00214ae8c9
Pavan Ahirwar <91248254+Sharad8518@users.noreply.github.com>
2022-08-30 01:25:52 +0530

    dash board desige

---
+++ src/screens/Delivery.js
@@ -0,0 +1,35 @@
+import { View, Text , TouchableOpacity ,Image} from 'react-native'
+import React from 'react'
+import {Card} from "react-native-paper"
+
+export default function Delivery() {
+  return (
+    <View>
+
+      <View style={{flexDirection:"row",justifyContent:"center",marginTop:20}}>
+      <Text style={{color:"#000"}}>Delivery Boy</Text>
+      </View>
+     <View style={{flexDirection:"row",justifyContent:"space-between",marginTop:20}}>
+
+     <Card style={{width:"45%",height:"100%",marginLeft:10}}>
+      <TouchableOpacity>
+      <View style={{flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
+
+<Image source={{uri:"https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Clipart.png"}}  style={{width:100,height:100,marginTop:10}}/>
+    <Text style={{marginBottom:10,color:"#000"}}>All Delivery Boy</Text>
+      </View>
+      </TouchableOpacity>
+     </Card>
+     <Card style={{width:"45%",height:"100%",marginRight:10,}}>
+     <View style={{flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
+     <Image source={{uri:"https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Clipart.png"}}  style={{width:100,height:100,marginTop:10}}/>
+    <Text style={{color:"#000"}}>Add Delivery Boy</Text>
+      </View>
+     </Card>
+
+     </View>
+    
+
+    </View>
+  )
+}
--- src/screens/Vendor.js
+++ src/screens/Vendor.js
@@ -1,14 +1,12 @@
 import { View, Text , TouchableOpacity ,Image} from 'react-native'
 import React from 'react'
 import {Card} from "react-native-paper"
-
-
-export default function AdminDashboard() {
+export default function Vendor() {
   return (
     <View>
 
-      <View style={{flexDirection:"row",justifyContent:"center",marginTop:100}}>
-      <Text style={{color:"#000"}}>Admin Dashboard</Text>
+      <View style={{flexDirection:"row",justifyContent:"center",marginTop:20}}>
+      <Text style={{color:"#000"}}>Vendor</Text>
       </View>
      <View style={{flexDirection:"row",justifyContent:"space-between",marginTop:20}}>
 
@@ -17,14 +15,14 @@ export default function AdminDashboard() {
       <View style={{flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
 
 <Image source={{uri:"https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Clipart.png"}}  style={{width:100,height:100,marginTop:10}}/>
-    <Text style={{marginBottom:10,color:"#000"}}>All Vendor</Text>
+    <Text style={{marginBottom:10,color:"#000"}}>All Urban</Text>
       </View>
       </TouchableOpacity>
      </Card>
      <Card style={{width:"45%",height:"100%",marginRight:10,}}>
      <View style={{flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
      <Image source={{uri:"https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Clipart.png"}}  style={{width:100,height:100,marginTop:10}}/>
-    <Text style={{color:"#000"}}>All Service</Text>
+    <Text style={{color:"#000"}}>All Rural</Text>
       </View>
      </Card>
 
@@ -36,14 +34,14 @@ export default function AdminDashboard() {
  <View style={{flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
 
 <Image source={{uri:"https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Clipart.png"}}  style={{width:100,height:100,marginTop:10}}/>
-<Text style={{marginBottom:10,color:"#000"}}> All Delivery Boy</Text>
+<Text style={{marginBottom:10,color:"#000"}}> All Order</Text>
  </View>
  </TouchableOpacity>
 </Card>
 <Card style={{width:"45%",height:"100%",marginRight:10,}}>
 <View style={{flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
 <Image source={{uri:"https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Clipart.png"}}  style={{width:100,height:100,marginTop:10}}/>
-<Text style={{color:"#000"}}>All User</Text>
+<Text style={{color:"#000"}}>All Cancel Order</Text>
  </View>
 </Card>
 
